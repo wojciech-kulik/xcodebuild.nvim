07d72cc9e7ec1cefad9770d5894f880c3dc97c40
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2023-11-25 20:03:51 +0100

    Fixed snapshots processing when build is cancelled

--- lua/xcodebuild/snapshots.lua
+++ lua/xcodebuild/snapshots.lua
@@ -5,6 +5,13 @@ local appdata = require("xcodebuild.appdata")
 local M = {}
 
 function M.save_failing_snapshots(reportPath, callback)
+  if not reportPath then
+    if callback then
+      callback()
+    end
+    return
+  end
+
   local savePath = appdata.snapshots_dir
   local getsnapshotPath = appdata.tool_path(GETSNAPSHOTS_TOOL)
   local command = getsnapshotPath .. " '" .. reportPath .. "' '" .. savePath .. "'"
