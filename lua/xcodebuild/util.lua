00f744d650eac06ee81d821369fef187d3bdbbd9
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2023-11-27 17:24:34 +0100

    Support for multiple failures within a single test

--- lua/xcodebuild/util.lua
+++ lua/xcodebuild/util.lua
@@ -1,5 +1,19 @@
 local M = {}
 
+function M.shallow_copy(orig)
+  local orig_type = type(orig)
+  local copy
+  if orig_type == "table" then
+    copy = {}
+    for orig_key, orig_value in pairs(orig) do
+      copy[orig_key] = orig_value
+    end
+  else -- number, string, boolean, etc
+    copy = orig
+  end
+  return copy
+end
+
 function M.is_empty(tab)
   return next(tab or {}) == nil
 end
