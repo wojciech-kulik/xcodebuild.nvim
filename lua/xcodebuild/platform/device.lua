08555919d07537857ecfb25d1cd3f34551130374
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2024-09-13 00:33:34 +0200

    fix: improved launching macOS app (#176)

--- lua/xcodebuild/platform/device.lua
+++ lua/xcodebuild/platform/device.lua
@@ -40,7 +40,7 @@ local function launch_app(waitForDebugger, callback)
     if waitForDebugger then
       return macos.launch_and_debug(settings.appPath, finished)
     else
-      return macos.launch_app(settings.appPath, settings.productName, finished)
+      return macos.launch_app(settings.appPath, finished)
     end
   end
 
